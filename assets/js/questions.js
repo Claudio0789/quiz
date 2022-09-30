8930a32516f1307cbda2d087d2093547f87c8597
Claudio0789 <claudiosfj@live.com>
2022-09-30 14:02:19 -0300

    first commit

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        question: 'Em qual elemento HTML nós colocamos o javascript?',
+        options: [
+            'tag js',
+            'tag script',
+            'tag scripting',
+            'tag javascript'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?',
+        options: [
+            'name="xxx.js"',
+            'src="xxx.js"',
+            'href="xxx.js"',
+            'style="xxx.js"'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Como escrever um "Olá Mundo" em um alertbox?',
+        options: [
+            'alertBox("Olá Mundo")',
+            'msgBox("Olá Mundo")',
+            'msg("Olá Mundo")',
+            'alert("Olá Mundo")'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Como escrever uma condicional IF no Javascript?',
+        options: [
+            'if i = 5',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i != 5 then'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Como fazer um if que executa um código caso "i" for diferente de 5',
+        options: [
+            'if (i != 5)',
+            'if (i <> 5)',
+            'if i <> 5',
+            'if i =! 5 then'
+        ],
+        answer: 0
+    }
+];
